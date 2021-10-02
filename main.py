822ea6c4342acfeccac1e416672ce4f72f6f768d
Mateusz Kozlowski <ct14kozlowskim@gmail.com>
2021-10-02 12:45:32 +0200

    base page theme added

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "default_secret_key"
 
 
 @app.route("/")
 def home_page():
-    return "Hello pomelo"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
