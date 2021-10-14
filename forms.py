eb4dda018ea44cab7591005235ffb9149a16c338
Mateusz Kozlowski <ct14kozlowskim@gmail.com>
2021-10-14 14:33:25 +0200

    deleted result page, changed encoder logic

--- forms.py
+++ forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField,TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError
 
 
 class UserInput(FlaskForm):
-    text_input = TextAreaField("Your message", validators=[DataRequired()])
-    submit_button = SubmitField("Encode message")
+    text_input = TextAreaField("Input", validators=[DataRequired()])
+    text_output = TextAreaField("Output")
+    encode_button = SubmitField("Encode")
+    decode_button = SubmitField("Decode")
--- main.py
+++ main.py
@@ -1,35 +1,26 @@
-from flask import Flask, render_template, redirect, url_for,session
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from forms import UserInput
-from encoder import Encoder
+from encoder import encode_message,allowed_to_encode
 
 # setup app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "default_secret_key"
 Bootstrap(app)
-encoder = Encoder()
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     input_form = UserInput()
     if input_form.validate_on_submit():
-        text_to_encode = input_form.text_input.data
-        if encoder.is_proper_message(text_to_encode):
-            session['text_to_encode'] = text_to_encode
-            return redirect(url_for('result'))
-        else:
-            input_form.text_input.errors.append("Not allowed symbols used")
-    return render_template("home.html", form=input_form)
-
-
-@app.route("/result")
-def result():
-    text = session.pop('text_to_encode', None)
-    if text is None:
-        return redirect(url_for('home'))
-    encoded_text = encoder.encode_message(text)
-    return render_template("result.html", text=text, encoded_text=encoded_text)
+        text = input_form.text_input.data
+        if input_form.encode_button.data:
+            if allowed_to_encode(text):
+                encoded_text = encode_message(text)
+                return render_template("home.html", form=input_form, result=encoded_text)
+            else:
+                input_form.text_input.errors.append("Unallowed letters used.")
+    return render_template("home.html", form=input_form, result="")
 
 
 if __name__ == "__main__":
