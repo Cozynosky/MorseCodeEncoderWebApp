22fb48a5bf833805064f97c87f6dc1715dc75bdd
Mateusz Kozlowski <ct14kozlowskim@gmail.com>
2021-10-16 18:02:26 +0200

    added creating .mp3 files with playing it option

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from forms import UserInput
 from encoder import encode_message, allowed_to_encode, allowed_to_decode, decode_message
+from make_audio import make_audio
 
 # setup app
 app = Flask(__name__)
@@ -17,17 +18,18 @@ def home():
         if input_form.encode_button.data:
             if allowed_to_encode(text):
                 encoded_text = encode_message(text)
-                return render_template("home.html", form=input_form, result=encoded_text)
+                file_path = make_audio(encoded_text)
+                return render_template("home.html", form=input_form, result=encoded_text, audio_file=file_path)
             else:
                 input_form.text_input.errors.append("Unallowed letters for encoding used.")
         elif input_form.decode_button.data:
-            words = text.split(" / ")
+            words = text.strip().split(" / ")
             if allowed_to_decode(words):
                 decoded_text = decode_message(words)
-                return render_template("home.html", form=input_form, result=decoded_text)
+                return render_template("home.html", form=input_form, result=decoded_text, audio_file=False)
             else:
                 input_form.text_input.errors.append("Unallowed symbols for decoding used.")
-    return render_template("home.html", form=input_form, result="")
+    return render_template("home.html", form=input_form, result="", audio_file=False)
 
 
 if __name__ == "__main__":
---
+++ make_audio.py
@@ -0,0 +1,24 @@
+import datetime
+from pydub import AudioSegment
+
+_DI_SOUND = AudioSegment.from_file("static/sounds/di_sound.wav", format="wav")
+_DAH_SOUND = AudioSegment.from_file("static/sounds/dah_sound.wav", format="wav")
+
+_PAUSE_SOUND = AudioSegment.silent(len(_DI_SOUND))
+
+
+def make_audio(text):
+    filename = datetime.datetime.now().strftime("%y%m%d%H%M%S%f")
+    output_sound = AudioSegment.silent(duration=0)
+
+    for sign in text:
+        if sign == '.':
+            output_sound += _DI_SOUND + _PAUSE_SOUND
+        elif sign == '-':
+            output_sound += _DAH_SOUND + _PAUSE_SOUND
+        elif sign == ' ' or sign == '/':
+            output_sound += _PAUSE_SOUND + _PAUSE_SOUND
+
+    output_sound.export(f"static/tmp/{filename}.mp3", format="mp3")
+
+    return f"tmp/{filename}.mp3"
